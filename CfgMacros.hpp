1528c33802ba08e14e68ca2867bb28e155129210
ElTyranos <pierre.cazard@gmail.com>
2016-11-02 16:37:09 +0100

    ~ Added: Macros

---
+++ CfgMacros.hpp
@@ -0,0 +1,240 @@
+// linkedItems[]
+#define LINKED_ITEMS_BASE "ItemMap","ItemCompass","ItemWatch"
+#define LINKED_ITEMS_COMMANDER "ItemRadio","ItemMap","ItemCompass","ItemWatch"
+
+
+// Items[]
+#define ITEMS_BASE "ACE_fieldDressing","ACE_fieldDressing","ACE_fieldDressing","ACE_fieldDressing","ACE_morphine","ACE_MapTools","ACE_Flashlight_MX991","ACE_earplugs"
+
+
+// Magazines[]
+
+// Grenades
+#define SOV_GRENADES "LIB_f1","LIB_f1"
+#define SOV_GRENADES_SMOKE "LIB_RDG","LIB_RDG"
+
+#define US_GRENADES "LIB_US_Mk_2","LIB_US_Mk_2"
+#define US_GRENADES_SMOKE "LIB_US_M18","LIB_US_M18_Red"
+
+#define GER_GRENADES "LIB_shg24","LIB_shg24"
+#define GER_GRENADES_SMOKE "LIB_nb39","LIB_nb39"
+
+// GER
+#define GER_20x_lib_5Rnd_792x57 "lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57"
+
+#define GER_10x_lib_5Rnd_792x57 "lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57","lib_5Rnd_792x57"
+
+#define GER_10x_lib_10Rnd_792x57 "lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57","lib_10Rnd_792x57"
+
+#define GER_7x_lib_32Rnd_9x19 "lib_32Rnd_9x19","lib_32Rnd_9x19","lib_32Rnd_9x19","lib_32Rnd_9x19","lib_32Rnd_9x19","lib_32Rnd_9x19","lib_32Rnd_9x19"
+
+#define GER_3x_lib_32Rnd_9x19 "lib_32Rnd_9x19","lib_32Rnd_9x19","lib_32Rnd_9x19"
+
+#define GER_7x_LIB_30Rnd_792x33 "LIB_30Rnd_792x33","LIB_30Rnd_792x33","LIB_30Rnd_792x33","LIB_30Rnd_792x33","LIB_30Rnd_792x33","LIB_30Rnd_792x33","LIB_30Rnd_792x33"
+
+#define GER_3x_LIB_50Rnd_792x57 "LIB_50Rnd_792x57","LIB_50Rnd_792x57","LIB_50Rnd_792x57"
+
+#define GER_2x_LIB_8Rnd_9x19 "LIB_8Rnd_9x19","LIB_8Rnd_9x19"
+
+
+// SOV
+#define SOV_20x_LIB_5Rnd_762x54 "LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54"
+
+#define SOV_10x_LIB_5Rnd_762x54 "LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54","LIB_5Rnd_762x54"
+
+#define SOV_10x_LIB_10Rnd_762x54 "LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54","LIB_10Rnd_762x54"
+
+#define SOV_6x_LIB_35Rnd_762x25 "LIB_35Rnd_762x25","LIB_35Rnd_762x25","LIB_35Rnd_762x25","LIB_35Rnd_762x25","LIB_35Rnd_762x25","LIB_35Rnd_762x25","LIB_35Rnd_762x25"
+
+#define SOV_3x_LIB_35Rnd_762x25 "LIB_35Rnd_762x25","LIB_35Rnd_762x25","LIB_35Rnd_762x25"
+
+#define SOV_3x_LIB_71Rnd_762x25 "LIB_71Rnd_762x25","LIB_71Rnd_762x25","LIB_71Rnd_762x25"
+
+#define SOV_2x_LIB_47Rnd_762x54 "LIB_47Rnd_762x54","LIB_47Rnd_762x54"
+
+#define SOV_2x_LIB_7Rnd_762x38 "LIB_7Rnd_762x38","LIB_7Rnd_762x38"
+
+#define SOV_3x_Lib_B32 "Lib_B32","Lib_B32","Lib_B32"
+
+
+// US
+#define US_13x_LIB_8Rnd_762x63 "LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63","LIB_8Rnd_762x63"
+
+#define US_13x_LIB_20Rnd_762x63 "LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63","LIB_20Rnd_762x63"
+
+#define US_10x_LIB_15Rnd_762x33 "LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33","LIB_15Rnd_762x33"
+
+#define US_7x_LIB_30Rnd_45ACP "LIB_30Rnd_45ACP","LIB_30Rnd_45ACP","LIB_30Rnd_45ACP","LIB_30Rnd_45ACP","LIB_30Rnd_45ACP","LIB_30Rnd_45ACP","LIB_30Rnd_45ACP"
+
+#define US_10x_LIB_5Rnd_762x63 "LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63","LIB_5Rnd_762x63"
+
+
+// Vehicles common
+#define COMMON_VHL_TRANSPORT_TOOLKIT \
+	class TransportItems \
+	{ \
+		class _xx_ToolKit \
+		{ \
+			name = "ToolKit"; \
+			count = 1; \
+		}; \
+	};
+
+#define COMMON_VHL_TRANSPORT_ITEMS \
+	class TransportItems \
+	{ \
+		class _xx_ToolKit \
+		{ \
+			name = "ToolKit"; \
+			count = 1; \
+		}; \
+		class _xx_ACE_HandFlare_White \
+		{ \
+			name = "ACE_HandFlare_White"; \
+			count = 2; \
+		}; \
+		class _xx_ACE_fieldDressing \
+		{ \
+			count = 14; \
+			name = "ACE_fieldDressing"; \
+		}; \
+		class _xx_ACE_morphine \
+		{ \
+			count = 7; \
+			name = "ACE_morphine"; \
+		}; \
+	};
+
+#define ARMORED_VHL_TRANSPORT_ITEMS \
+	class TransportItems \
+	{ \
+		class _xx_ToolKit \
+		{ \
+			name = "ToolKit"; \
+			count = 1; \
+		}; \
+		class _xx_ACE_HandFlare_White \
+		{ \
+			name = "ACE_HandFlare_White"; \
+			count = 2; \
+		}; \
+		class _xx_ACE_fieldDressing \
+		{ \
+			count = 6; \
+			name = "ACE_fieldDressing"; \
+		}; \
+		class _xx_ACE_morphine \
+		{ \
+			count = 3; \
+			name = "ACE_morphine"; \
+		}; \
+	}; \
+	class TransportMagazines { \
+		class _xx_LIB_1Rnd_flare_red  \
+		{ \
+			magazine = "LIB_1Rnd_flare_red"; \
+			count = 2; \
+		}; \
+		class _xx_LIB_1Rnd_flare_green  \
+		{ \
+			magazine = "LIB_1Rnd_flare_green"; \
+			count = 2; \
+		}; \
+		class _xx_LIB_1Rnd_flare_yellow  \
+		{ \
+			magazine = "LIB_1Rnd_flare_yellow"; \
+			count = 2; \
+		}; \
+	}; \
+	class TransportWeapons { \
+		class _xx_LIB_LIB_FLARE_PISTOL \
+		{ \
+			weapon = "LIB_FLARE_PISTOL"; \
+			count = 1; \
+		}; \
+	}; \
+	class TransportBackpacks {};
+	
+// side vehicles
+#define SOV_VHL_TRANSPORT_WEAPONS \
+	class TransportMagazines { \
+		class _xx_LIB_47Rnd_762x54 \
+		{ \
+			magazine = "LIB_47Rnd_762x54"; \
+			count = 10; \
+		}; \
+		class _xx_LIB_rpg6 \
+		{ \
+			magazine = "LIB_rpg6"; \
+			count = 3; \
+		}; \
+		class _xx_LIB_rg42 \
+		{ \
+			magazine = "LIB_rg42"; \
+			count = 3; \
+		}; \
+		class _xx_LIB_RDG \
+		{ \
+			magazine = "LIB_RDG"; \
+			count = 3; \
+		}; \
+	}; \
+	class TransportWeapons {}; \
+	class TransportBackpacks {};
+
+#define US_VHL_TRANSPORT_WEAPONS \
+	class TransportMagazines { \
+		class _xx_LIB_20Rnd_762x63 \
+		{ \
+			magazine = "LIB_20Rnd_762x63"; \
+			count = 10; \
+		}; \
+		class _xx_LIB_rpg6 \
+		{ \
+			magazine = "LIB_rpg6"; \
+			count = 3; \
+		}; \
+		class _xx_LIB_US_Mk_2 \
+		{ \
+			magazine = "LIB_US_Mk_2"; \
+			count = 3; \
+		}; \
+		class _xx_LIB_US_M18 \
+		{ \
+			magazine = "LIB_US_M18"; \
+			count = 2; \
+		}; \
+		class _xx_LIB_US_M18_Red \
+		{ \
+			magazine = "LIB_US_M18_Red"; \
+			count = 2; \
+		}; \
+	}; \
+	class TransportWeapons {}; \
+	class TransportBackpacks {};
+
+#define GER_VHL_TRANSPORT_WEAPONS \
+	class TransportMagazines { \
+		class _xx_LIB_50Rnd_792x57 \
+		{ \
+			magazine = "LIB_50Rnd_792x57"; \
+			count = 10; \
+		}; \
+		class _xx_LIB_pwm \
+		{ \
+			magazine = "LIB_pwm"; \
+			count = 3; \
+		}; \
+		class _xx_LIB_shg24x7 \
+		{ \
+			magazine = "LIB_shg24x7"; \
+			count = 2; \
+		}; \
+		class _xx_LIB_nb39 \
+		{ \
+			magazine = "LIB_nb39"; \
+			count = 3; \
+		}; \
+	}; \
+	class TransportWeapons {}; \
+	class TransportBackpacks {};
