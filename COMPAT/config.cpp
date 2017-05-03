92ccc237c4a912acc12d7ecdca161e59c3931e16
ElTyranos <pierre.cazard@gmail.com>
2017-05-03 20:28:50 +0200

    ~ Added: backward compatibility after airborne update

---
+++ COMPAT/config.cpp
@@ -0,0 +1,13 @@
+// Missions backward compatibility
+class CfgPatches
+{
+	class if_weapon1
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+	class BG21_WW2_Assets: if_weapon1 {};
+	class BG21_WW2_c: if_weapon1 {};
+};
