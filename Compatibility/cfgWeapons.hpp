47ff6f23bf5e137bd29993bbc885461446f827ec
ElTyranos <pierre.cazard@gmail.com>
2017-06-06 04:01:25 +0200

    ~ Improved: UK vests capacity

--- Compatibility/cfgWeapons.hpp
+++ Compatibility/cfgWeapons.hpp
@@ -1,12 +1,128 @@
 // FOW Vests
 class ItemCore;
-class Vest_Camo_Base: ItemCore {
+class Vest_Camo_Base;
+class fow_v_base: Vest_Camo_Base {
 	class ItemInfo;
 };
-class fow_v_base: Vest_Camo_Base {
+class fow_v_uk_officer: fow_v_base
+{
 	class ItemInfo: ItemInfo
 	{
-		containerClass = "Supply70";
+		containerClass = "Supply100";
+	};
+};
+class fow_v_uk_base: fow_v_base
+{	
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_uk_bren: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_uk_sten: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_uk_para_base: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_uk_para_sten: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_uk_para_bren: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+
+class fow_v_heer_p38: fow_v_base
+{	
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_g43: fow_v_base
+{	
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_k98: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_k98_ass: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+
+class fow_v_heer_k98_light: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_mg: fow_v_base
+{	
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_mp40: fow_v_base
+{	
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_mp40_nco: fow_v_base
+{	
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_heer_mp44: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
+	};
+};
+class fow_v_fall_bandoleer: fow_v_base
+{
+	class ItemInfo: ItemInfo
+	{
+		containerClass = "Supply100";
 	};
 };
 
