315a25fa30ae63197b7744878ad681f36575e5ae
ElTyranos <pierre.cazard@gmail.com>
2019-05-25 18:35:33 +0200

    ~ Fixed: preInit function names.

--- Extended_PreInit_EventHandlers.hpp
+++ Extended_PreInit_EventHandlers.hpp
@@ -1,7 +1,11 @@
 class Extended_PreInit_EventHandlers
 {
-	class BG21_OutOfAera_Settings
+	class BG21_fnc_OutofAera_Client
 	{
-		serverInit = "call compile preProcessFileLineNumbers '\WW2OG_AllInOne\fnc\fn_OutofAera_PreInit.sqf'";
+		clientInit = "call compile preProcessFileLineNumbers '\WW2OG_AllInOne\fnc\fn_OutofAera_client.sqf'";
+	};
+	class BG21_fnc_OutofAera_Server
+	{
+		serverInit = "call compile preProcessFileLineNumbers '\WW2OG_AllInOne\fnc\fn_OutofAera_server.sqf'";
 	};
 };
