293c2b9e7cb921198caed4ea12df0c3f23f7142d
ElTyranos <pierre.cazard@gmail.com>
2019-05-25 03:17:57 +0200

    ~ Removed: deprecated code.

--- Compatibility/cfgWeapons.hpp
+++ Compatibility/cfgWeapons.hpp
@@ -182,49 +182,4 @@ class fow_v_ija_mortar: fow_v_base
 		containerClass = "Supply100";
 	};
 };
-
-// PIAT ACE3 Compat
-class Launcher_Base_F;
-class fow_w_piat: Launcher_Base_F
-{
-	ace_overpressure_angle = 0;
-	ace_overpressure_range = 0;
-	ace_overpressure_damage = 0;
-	ace_reloadlaunchers_enabled = 1;
-};
-class fow_w_m1a1_bazooka : Launcher_Base_F
-{
-	ace_overpressure_angle = 60;
-	ace_overpressure_range = 10;
-	ace_overpressure_damage = 0.4;
-	ace_reloadlaunchers_enabled = 1;
-};
-
-// LEN Sounds
-class LIB_RIFLE;
-class LIB_K98: LIB_RIFLE {
-	class Single;
-};
-class LEN_SMLE_No4Mk1: LIB_K98 {
-	class Single : Single {
-		reloadTime = 1;
-		dispersion = 0.0008;
-		aiDispersionCoefX = 7;
-		aiDispersionCoefY = 7;
-		aiRateOfFire = 0.001;
-		aiRateOfFireDistance = 10;
-		minRange = 0;
-		minRangeProbab = 0.2;
-		midRange = 5;
-		midRangeProbab = 0.7;
-		maxRange = 10;
-		maxRangeProbab = 0.05;
-		
-		sounds[] = {"StandardSound"};
-		class BaseSoundModeType;
-		class StandardSound : BaseSoundModeType {
-			soundSetShot[] = {"FoW_SMLE_Firing", "FoW_Rifle_EFX"};
-		};
-	};
-};
 */
