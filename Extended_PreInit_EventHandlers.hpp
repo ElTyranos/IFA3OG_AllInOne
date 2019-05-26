ffa02af4885dafa2fdd09a1769c226c888499331
ElTyranos <pierre.cazard@gmail.com>
2019-05-26 13:06:20 +0200

    ~ Changed: moved from pre to post init.

--- Extended_PreInit_EventHandlers.hpp
+++ Extended_PreInit_EventHandlers.hpp
@@ -1,13 +1,5 @@
 class Extended_PreInit_EventHandlers
 {
-	class BG21_fnc_OutofAera_Client
-	{
-		clientInit = "call compile preProcessFileLineNumbers '\WW2OG_AllInOne\fnc\fn_OutofAera_client.sqf'";
-	};
-	class BG21_fnc_OutofAera_Server
-	{
-		serverInit = "call compile preProcessFileLineNumbers '\WW2OG_AllInOne\fnc\fn_OutofAera_server.sqf'";
-	};
 	// class BG21_fnc_Log
 	// {
 		// serverInit = "call compile preProcessFileLineNumbers '\WW2OG_AllInOne\fnc\fn_log.sqf'";
